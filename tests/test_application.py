8bf3bd8e91b1afdfd50be77dd553f27623ea6d90
Stefano Gallotti <sgallotti@expediagroup.com>
2020-09-20 12:03:25 -0700

    exit loop

--- tests/test_application.py
+++ tests/test_application.py
@@ -4,7 +4,7 @@ from src.application import menu
 
 def test_menu():
 
-    with patch("builtins.input", return_value="1"):
+    with patch("builtins.input", side_effect=["1", "q"]):
         with patch("src.functions.getHistoricalQuotes") as mock:
             selection = menu()
             mock.assert_called()
